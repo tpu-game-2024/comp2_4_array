1db6c54e96c811a8116248eff12bd34755f40700
hashimoto0517 <izumi.gsmz1@gmail.com>
2024-12-16 15:14:27 +0900

    テストを通しました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -34,28 +34,62 @@ void release(my_array* ar)
 // my_array の要素数を変更する(以前の値は要素が同じ範囲で残す)
 void resize(my_array* ar, int n)
 {
-	// ToDo:配列の要素数を変更しよう！(reallocは禁止)
+	if (ar == NULL)
+		return;// NULLチェック
+
+	int* newar = (int*)malloc(sizeof(int) * n);//新しい要素数の配列作成
+	if (newar == NULL)
+		return; 
+
+	int min;
+	if (n < ar->num)
+		min = n;
+	else
+		min = ar->num;
+	
+	if (ar->addr != NULL) {
+		memcpy_s(newar, sizeof(int) * n, 
+			ar->addr, sizeof(int) * min);
+	}
+	
+	ar->addr = newar;
+	ar->num = n;
 }
 
 // my_array のindex番目の要素にvalを設定する
 // index が確保されていない場所を指していたら返り値をfalseにする（設定出来たらtrue）
 bool set(my_array* ar, int index, int val)
 {
-	// ToDo:配列の要素を変更しよう！
-	return false;
+	if (ar == NULL || ar->addr == NULL) //NULLチェック
+		return false;
+
+	if (index < 0 || index >= ar -> num)//indexが確保されていない場所を指すとき
+		return false;
+	
+	ar->addr[index] = val;
+	return true;
 }
 
 // my_array のindex番目の要素を取得する
 // index が確保されていない場所を指していたら0を返す
 int get(const my_array* ar, int index)
-{
-	// ToDo:要素を所得して、indexがおかしかったら0を返そう
-	return -1;
+{	// ToDo:要素を所得して、indexがおかしかったら0を返そう
+
+	if (ar == NULL || ar->addr == NULL) //NULLチェック
+		return 0;
+
+	if (index < 0 || index >= ar->num)//indexが確保されていない場所を指すとき
+		return 0;
+
+	return ar->addr[index];
 }
 
 // my_array の要素数を取得する
 int size(const my_array* ar)
 {
 	// ToDo: 配列の要素数を返そう
-	return -1;
+	if(ar == NULL|| ar->addr == NULL)
+		return 0;
+
+	return ar->num;
 }
